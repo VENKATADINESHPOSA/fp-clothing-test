47c637672592eeeb39a8761c55e4c284eb61b2a5
VENKATADINESHPOSA <posavenkatadinesh@gmail.com>
2021-07-23 12:02:58 +0530

    [Dinesh] - Added Routing to the menu-item with HOC(withRouter())

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <>
+      <Link to="/">HomePage</Link>
+      <h1>HATS PAGE</h1>
+      <button onClick={() => props.history.push("/")}>HomePage</button>
+    </>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
